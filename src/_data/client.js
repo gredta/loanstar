ce36accb48b62b9bc201650403c7c3463608b274
greta <greta.belegu@gmail.com>
2026-01-01 22:43:37 -0500

    initial commit

--- src/_data/client.js
+++ src/_data/client.js
@@ -1,23 +1,21 @@
 module.exports = {
-    name: "Code Stitch Web Designs",
-    email: "help@codestitch.app",
-    phoneForTel: "555-779-4407",
-    phoneFormatted: "(555) 779-4407",
+    name: "Loanstar INC.",
+    email: "loanstarinc1@gmail.com",
+    phoneForTel: "718-820-2561",
+    phoneFormatted: "(718) 820-2561",
     address: {
-        lineOne: "First Address Line",
-        lineTwo: "Second Address Line",
-        city: "Denver",
-        state: "CO",
-        zip: "80206",
+        lineOne: "62-84 Booth Street",
+        city: "Rego Park",
+        state: "NY",
+        zip: "11374",
         country: "US",
-        mapLink: "https://maps.app.goo.gl/TEdS5KoLC9ZcULuQ6",
     },
     socials: {
         facebook: "https://www.facebook.com/",
         instagram: "https://www.instagram.com/",
     },
     //! Make sure you include the file protocol (e.g. https://) and that NO TRAILING SLASH is included
-    domain: "https://www.example.com",
+    domain: "https://www.loanstarinc.com",
     // Passing the isProduction variable for use in HTML templates
     isProduction: process.env.ELEVENTY_ENV === "PROD",
 };
